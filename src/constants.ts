972696944759f8d459536131d7b80c684f09da84
jpcrs <joaop.carrasco@gmail.com>
2022-07-05 18:58:16 +0100

    Fix macos external shell command

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const EXTENSION_NAME = 'binocular';
 
 export enum ExternalTerminalCommands {
     windows = `Start-Process PowerShell -ArgumentList "#"`,
-    macOs = `osascript -e 'tell app "Terminal" to do script "ls" & activate & do script "#;exit"`,
+    macOs = `osascript -e "tell app \"Terminal\" to activate & do script \"#;exit\""`,
     linux = `x-terminal-emulator -- sh -c "#"`
 }
